88d5f0f47f50b322c5fede6cc8786f11c691909c
alessiaca <49674790+alessiaca@users.noreply.github.com>
2021-11-23 18:38:25 +0100

    more updates

--- validation/project_code/optimal_closed_loop_system.py
+++ validation/project_code/optimal_closed_loop_system.py
@@ -4,40 +4,47 @@ import numpy as np
 import mne
 import matplotlib.pyplot as plt
 from scipy.signal import hilbert, filtfilt
+from scipy.io import loadmat
+from mne.time_frequency import psd_array_welch
 from main_code.utils import compute_P,fir_coeffs,get_audio_events,get_audio_phases, plv, find_bad_channels
 
 # Load the data used for simulation
 folder_path = "C:/Users/alessia/Documents/Jobs/CNT/CL-amtACS/validation/data/16_11_21/16_11_21/"
 raw_no_stim = mne.io.read_raw_brainvision(folder_path + '/no_stim/no_stim_1.vhdr', preload=True)
 raw_stim = mne.io.read_raw_brainvision(folder_path + '/corr/stim.vhdr', preload=True)
+initial_bad_channels = loadmat(folder_path + "/exclude_idx.mat")["exclude_idx"][0]
+initial_bad_channels = [raw_stim.ch_names[i] for i in initial_bad_channels - 1]
+
+# Downsample the data
+raw_stim.resample(500)
+raw_no_stim.resample(500)
 sfreq = raw_no_stim.info['sfreq']
 
+# Crop the dataset for faster debugging
 raw_stim.crop(0,60*3)
 raw_no_stim.crop(0,60*3)
 
 # Set some parameters
 frequency = 6
 lfreq = frequency - 1; hfreq = frequency + 1
-
 buffer_size = sfreq * 1
 buffer_SASS_size = sfreq * 25
 buffer_SASS_step_size = sfreq * 1
 coeffs = fir_coeffs(frequency, n_taps=84)
-coeffs_SASS = fir_coeffs(frequency, n_taps=42)
 
 # Get the audio channel
 audio_no_stim = raw_no_stim.get_data("tones").flatten()
 audio_stim = raw_stim.get_data(["tones"]).flatten()
-audio_stim_events = get_audio_events(audio_stim,sfreq)
-audio_stim_phases = get_audio_phases(audio_stim_events, len(audio_stim), sfreq)
-audio_no_stim_events = get_audio_events(audio_no_stim, sfreq)
-audio_no_stim_phases = get_audio_phases(audio_no_stim_events, len(audio_no_stim), sfreq)
+audio_stim_phases = get_audio_phases(audio_stim)
+audio_no_stim_phases = get_audio_phases(audio_no_stim)
 
 # Drop non EEG and bad channels
-inital_bad_channels = ['env', 'stim', 'tones', 'sass chan']
+print(f"Bad channels initial {initial_bad_channels}")
+inital_bad_channels = ['env', 'stim', 'tones', 'sass chan'] + initial_bad_channels
 raw_no_stim.drop_channels([ch for ch in raw_no_stim.ch_names if ch in inital_bad_channels])
 raw_stim.drop_channels([ch for ch in raw_stim.ch_names if ch in inital_bad_channels])
 bad_channels = find_bad_channels(raw_stim)
+print(f"Bad channels new {bad_channels}")
 raw_no_stim.drop_channels(bad_channels)
 raw_stim.drop_channels(bad_channels)
 
@@ -48,17 +55,20 @@ chidx = ch_names.index("TP8")
 
 # Get the data used for simulation
 data_stim = raw_stim._data
-n_samps = data_stim.shape[1]
+data_no_stim = raw_no_stim._data
+n_samps_stim = data_stim.shape[1]
+n_samps_no_stim = data_no_stim.shape[1]
 
 # Create the covariance matrix of the data without stimulation (filtered around the target frequency)
-no_stim_data = filtfilt(coeffs_SASS, 1, raw_no_stim._data.copy())
-B = np.cov(no_stim_data)
+no_stim_data_filt = filtfilt(coeffs, 1, raw_no_stim._data.copy())
+B = np.cov(no_stim_data_filt)
 
 # Loop over the samples and apply SASS just as in the real time system
-steps = np.arange(buffer_SASS_size, n_samps,buffer_SASS_step_size)
+steps = np.arange(buffer_SASS_size, n_samps_stim-buffer_size, buffer_SASS_step_size)
 n_steps = len(steps)
 plvs_stim_final = np.zeros((n_steps,1))
 n_nulls_final = np.zeros((n_steps,1))
+target_chan_SASS = np.zeros((n_samps_stim,1))
 for i, i_samp in enumerate(steps):
 
     # Print status update
@@ -67,8 +77,10 @@ for i, i_samp in enumerate(steps):
     # Update the SASS Matrix
     # Get the buffer
     buffer_long = data_stim[:, int(i_samp - buffer_SASS_size):int(i_samp)].copy()
-    # Filter the buffer (with a longer filter)
-    buffer_long_filt = filtfilt(coeffs_SASS, 1, buffer_long.copy())
+    # Detrend the buffer
+    buffer_long -= buffer_long.mean(1)[:, np.newaxis]
+    # Filter the buffer
+    buffer_long_filt = filtfilt(coeffs, 1, buffer_long.copy())
     # Compute the covariance matrix
     A = np.cov(buffer_long_filt)
     # Compute the SASS Matrix for all n_nulls
@@ -77,11 +89,11 @@ for i, i_samp in enumerate(steps):
     # Apply the matrix for the next second (update rate of SASS)
 
     # Get the buffer (1 sec)
-    buffer = data_stim[:, int(i_samp - buffer_size):int(i_samp)].copy()
+    buffer = data_stim[:, int(i_samp):int(i_samp + buffer_size)].copy()
     # Detrend the buffer
     buffer -= buffer.mean(1)[:, np.newaxis]
     plv_all_SASS = np.zeros((n_chans,1))
-    for P_SASS,n_nulls in P_SASS_all:
+    for P_SASS, n_nulls in P_SASS_all:
         # Apply SASS
         buffer_SASS = P_SASS @ buffer
         # Compute the target channel
@@ -91,16 +103,32 @@ for i, i_samp in enumerate(steps):
 
         # Compute the phase locking of the target chan with the audio channel
         phases = np.angle(hilbert(target_chan_filt))
-        plv_all_SASS[n_nulls] = plv(phases - audio_stim_phases[int(i_samp - buffer_size):int(i_samp)])
+        plv_all_SASS[n_nulls] = plv(phases - audio_stim_phases[int(i_samp):int(i_samp + buffer_size)])
 
     plvs_stim_final[i] = np.max(plv_all_SASS)
     n_nulls_final[i] = np.argmax(plv_all_SASS)
 
-# What is the optimal plv? Without stimulation
-data_no_stim = raw_no_stim._data
-plvs_no_stim_final = np.zeros((len(steps),1))
+    # Save the target chan that was optimally cleaned
+    # Apply SASS
+    buffer_SASS = P_SASS_all[int(n_nulls_final[i][0])][0] @ buffer
+    # Compute the target channel
+    target_chan = buffer_SASS[chidx]
+    # Filter the target channel (with a short filter)
+    target_chan_filt = filtfilt(coeffs, 1, target_chan)
+    target_chan_SASS[int(i_samp):int(i_samp+buffer_size)] = target_chan_filt[:,np.newaxis]
+
+# Crop the cleaned data from the point it was cleaned
+target_chan_SASS = target_chan_SASS[int(buffer_SASS_size):]
+
+# Compute the plv without stimulation
+steps = np.arange(buffer_size, n_samps_stim, buffer_SASS_step_size)
+n_steps = len(steps)
+plvs_no_stim_final = np.zeros((n_steps,1))
 for i, i_samp in enumerate(steps):
+    # Get the buffer
     buffer = data_no_stim[:, int(i_samp - buffer_size):int(i_samp)].copy()
+    # Detrend the buffer
+    buffer -= buffer.mean(1)[:, np.newaxis]
     # Compute the target channel
     target_chan = buffer[chidx]
     # Filter the target channel (with a short filter)
@@ -109,8 +137,30 @@ for i, i_samp in enumerate(steps):
     phases = np.angle(hilbert(target_chan_filt))
     plvs_no_stim_final[i] = plv(phases - audio_no_stim_phases[int(i_samp - buffer_size):int(i_samp)])
 
+# Analyse the results
 plt.figure()
-plt.hist(plvs_no_stim_final, label="No stim")
-plt.hist(plvs_stim_final, label="Stim")
+plt.hist(plvs_stim_final, alpha=0.5, label="Stim")
+plt.hist(plvs_no_stim_final, alpha=0.5, label="No stim")
+plt.title("PLVs")
 plt.legend()
+plt.figure()
+plt.hist(n_nulls_final,n_chans)
+plt.title("Deleted n_nulls")
+
+# Plot the power spectrum of the target channel
+plt.figure()
+fmin = 1
+fmax = 50
+psds, freqs = psd_array_welch(target_chan_SASS.flatten(), fmin=fmin, fmax=fmax, sfreq=sfreq, n_fft=int(sfreq*5))
+plt.semilogy(freqs,psds,label="Stim after SASS")
+
+target_chan_no_stim = data_no_stim[chidx]
+target_chan_no_stim= filtfilt(coeffs, 1, target_chan_no_stim)
+psds, freqs = psd_array_welch(target_chan_no_stim.flatten(), fmin=fmin, fmax=fmax, sfreq=sfreq, n_fft=int(sfreq*5))
+plt.semilogy(freqs,psds,label="no stim")
+
+target_chan_stim = data_stim[chidx]
+target_chan_stim= filtfilt(coeffs, 1, target_chan_stim)
+psds, freqs = psd_array_welch(target_chan_stim, fmin=fmin, fmax=fmax, sfreq=sfreq, n_fft=int(sfreq*5))
+plt.semilogy(freqs,psds,label="Stim")
 plt.show()
--- validation/project_code/simulate_closed_loop_system.py
+++ validation/project_code/simulate_closed_loop_system.py
@@ -2,15 +2,28 @@
 
 import numpy as np
 import mne
+import matplotlib.pyplot as plt
 from scipy.signal import hilbert, filtfilt
+from scipy.io import loadmat
+from scipy.stats import  zscore
 from main_code.utils import DFT, compute_P, find_bad_channels,\
-      fir_coeffs,get_audio_events,get_audio_phases, plv
+      fir_coeffs,get_audio_events,get_audio_phases, plv, compute_phase
 
 # Load the data used for simulation
 folder_path = "C:/Users/alessia/Documents/Jobs/CNT/CL-amtACS/validation/data/16_11_21/16_11_21/"
 raw_no_stim = mne.io.read_raw_brainvision(folder_path + '/no_stim/no_stim_1.vhdr', preload=True)
 raw_stim = mne.io.read_raw_brainvision(folder_path + '/corr/stim.vhdr', preload=True)
-sfreq = raw_no_stim.info['sfreq']
+initial_bad_channels = loadmat(folder_path+"/exclude_idx.mat")["exclude_idx"][0]
+initial_bad_channels = [raw_stim.ch_names[i] for i in initial_bad_channels - 1]
+
+# Downsample the data to the rates used in the RL system (neglect the 250 Hz used for SASS computation)
+raw_stim.resample(500)
+raw_no_stim.resample(500)
+sfreq = raw_stim.info['sfreq']
+
+# Crop the data for faster debugging
+#raw_stim.crop(0,30*3)
+#raw_no_stim.crop(0,30*3)
 
 # Get the events (switches of phase shift)
 events_stim = mne.events_from_annotations(raw_stim)[0]
@@ -18,29 +31,31 @@ events_no_stim = mne.events_from_annotations(raw_no_stim)[0]
 
 # Set some parameters
 frequency = 6
-lfreq = frequency - 1; hfreq = frequency + 1
-
+lfreq = frequency - 1
+hfreq = frequency + 1
 buffer_size = sfreq * 1
-buffer_SASS_size = sfreq * 25
+buffer_SASS_size_sec = 25
+buffer_SASS_size = sfreq * buffer_SASS_size_sec
 buffer_SASS_step_size = sfreq * 1
 coeffs = fir_coeffs(frequency, n_taps=84)
-coeffs_SASS = fir_coeffs(frequency, n_taps=42)
+phase_shifts = [i/6 * 360 for i in range(6)]
 
 # Get the audio channel
+raw_audio_stim = raw_stim.copy().pick_channels(["tones"])   
 audio_no_stim = raw_no_stim.get_data("tones").flatten()
 audio_stim = raw_stim.get_data(["tones"]).flatten()
-audio_stim_events = get_audio_events(audio_stim,sfreq)
-audio_stim_phases = get_audio_phases(audio_stim_events, len(audio_stim), sfreq)
-audio_no_stim_events = get_audio_events(audio_no_stim, sfreq)
-audio_no_stim_phases = get_audio_phases(audio_no_stim_events, len(audio_no_stim) ,sfreq)
+audio_stim_phases = get_audio_phases(audio_stim)
+audio_no_stim_phases = get_audio_phases(audio_no_stim)
+
+# Get the envlopes
+raw_stim_env = raw_stim.copy().filter(lfreq,hfreq).pick_channels(["env"])
+raw_no_stim_env = raw_no_stim.copy().filter(lfreq,hfreq).pick_channels(["env"])
 
 # Drop non EEG and bad channels
-inital_bad_channels = ['env', 'stim', 'tones', 'sass chan']
+print(f"Bad channels initial {initial_bad_channels}")
+inital_bad_channels = ['env', 'stim', 'tones', 'sass chan'] + initial_bad_channels
 raw_no_stim.drop_channels([ch for ch in raw_no_stim.ch_names if ch in inital_bad_channels])
 raw_stim.drop_channels([ch for ch in raw_stim.ch_names if ch in inital_bad_channels])
-bad_channels = find_bad_channels(raw_stim)
-raw_no_stim.drop_channels(bad_channels)
-raw_stim.drop_channels(bad_channels)
 
 # Define the target channel
 ch_names = raw_stim.ch_names
@@ -52,24 +67,44 @@ data_stim = raw_stim._data
 n_samps = data_stim.shape[1]
 
 # Create the covariance matrix of the data without stimulation (filtered around the target frequency)
-no_stim_data = filtfilt(coeffs_SASS, 1, raw_no_stim._data.copy())
-B = np.cov(no_stim_data)
+no_stim_data_filt = filtfilt(coeffs, 1, raw_no_stim._data.copy())
+B = np.cov(no_stim_data_filt)
 
 # Loop over the samples and apply SASS just as in the real time system
 steps = np.arange(buffer_SASS_size, n_samps)
+n_steps = len(steps)
 envelope = np.zeros((n_samps, 1))
+phase_shift = 0
 for i, i_samp in enumerate(steps):
+
+    # Print status update
+    if i != 0 and i in np.round(np.linspace(0, n_steps, 10)): print(f"Done with {np.round((i / n_steps) * 100, 0)}%")
+
+    # Check current phase shift
+    if i_samp in events_stim[:,0]:
+        i_phase_shift = events_stim[np.where(events_stim[:,0] == i_samp)[0],2]
+        phase_shift = phase_shifts[int(i_phase_shift-1)]
 
     # Update the SASS Matrix
     if i_samp % buffer_SASS_step_size == 0:
-        # Get the buffer
+        # Get the buffer from the last 25 sec
         buffer_long = data_stim[:, int(i_samp - buffer_SASS_size):int(i_samp)].copy()
-        # Filter the buffer (with a longer filter)
-        buffer_long_filt = filtfilt(coeffs_SASS, 1, buffer_long.copy())
+        # Find bad channels and delete them
+        vars = np.var(buffer_long, 1)
+        idx_goods = np.abs(zscore(vars)) < 1.645
+        buffer_long = buffer_long[idx_goods, :]
+        # Detrend the buffer
+        buffer_long -= buffer_long.mean(1)[:, np.newaxis]
+        # Filter the buffer
+        buffer_long_filt = filtfilt(coeffs, 1, buffer_long.copy())
         # Compute the covariance matrix
         A = np.cov(buffer_long_filt)
         # Compute the SASS Matrix
-        P_SASS, n_nulls = compute_P(A, B)
+        tmp_B = B[np.ix_(idx_goods,idx_goods)]
+        P_SASS_small, n_nulls = compute_P(A, tmp_B)
+        # Embed in larger matrix (as channels were excluded)  ?? Correct ??
+        P_SASS = np.zeros((B.shape))
+        P_SASS[np.ix_(idx_goods,idx_goods)] = P_SASS_small
 
     # Apply the matrix to the new sample
 
@@ -80,14 +115,58 @@ for i, i_samp in enumerate(steps):
     # Apply SASS
     buffer_SASS = P_SASS @ buffer
     # Compute the target channel
-    target_chan = buffer_SASS(chidx)
+    target_chan = buffer_SASS[chidx]
     # Filter the target channel (with a short filter)
     target_chan_filt = filtfilt(coeffs, 1, target_chan.copy())
-    # Compute the phase using the DFT at the target frequency using the last 3 cycles
-    amplitude, phase = DFT(target_chan_filt[-int((sfreq / frequency) * 3):], frequency, sfreq)
-    # Transform the phase into an envelope
-    envelope[i] = np.cos(2 * np.pi * frequency * np.linspace(0, 1, 500) + phase)
+    # Compute the phase using the DFT at the target frequency using the last cycle
+    amplitude, phase = DFT(target_chan_filt[-int((sfreq / frequency) * 1):], frequency, sfreq)
+    # Transform the phase into an envelope  ?? Not correct??
+    envelope[int(i_samp)] = np.cos(2 * np.pi * frequency + phase - phase_shift)
+    # Low-pass-filter of the envelope??
 
-# Analyze the results
+# Create a new raw object using the simulated envelope
+raw_stim_env_sim = raw_stim_env.copy()
+raw_stim_env_sim._data = envelope.T
+
+# Crop the data as the first 25 sec cannot be used
+raw_stim_env_sim.crop(tmin=buffer_SASS_size_sec)
+raw_stim_env.crop(tmin=buffer_SASS_size_sec)
+raw_audio_stim.crop(tmin=buffer_SASS_size_sec)
+# Get the events (switches of phase shift)
+events_stim = mne.events_from_annotations(raw_stim_env)[0]
+# Include only events that are complete (20 sec)
+events_stim = events_stim[events_stim[:,0] + sfreq * 20 < raw_stim_env.last_samp]
+event_ids = np.unique(events_stim[:,2])
+
+# Analyze the results: Plot the phase difference between audio and envelope for real and simulated envelope
+fig_rt = plt.figure(1)
+fig_sim = plt.figure(2)
+for i,event_id in enumerate(event_ids):
+    # Create epochs
+    epochs_env = mne.Epochs(raw_stim_env, events_stim, event_id=event_id, tmin= 0, tmax=20, detrend=0, baseline=None,preload=True)
+    epochs_env_sim = mne.Epochs(raw_stim_env_sim, events_stim, event_id=event_id, tmin=0, tmax=20, detrend=0, baseline=None,preload=True, reject=None)
+    epochs_audio = mne.Epochs(raw_audio_stim, events_stim, event_id=event_id, tmin=0, tmax=20, detrend=0, baseline=None,preload=True)
+    # Calculate the phases of the audio in the epochs
+    phases_audio = get_audio_phases(epochs_audio._data.flatten())
+    # Calculate the phases of the envelopes
+    phases_env = compute_phase(epochs_env._data.flatten())
+    phases_env_sim = compute_phase(epochs_env_sim._data.flatten())
+    # Plot the phase differences
+    plt.figure(1)
+    plt.subplot(2, 3, i + 1, projection='polar')
+    plt.hist(phases_env - phases_audio, color="green", alpha=0.5, label="RT")
+    plt.title(event_id)
+    plt.figure(2)
+    plt.subplot(2, 3, i + 1, projection='polar')
+    plt.hist(phases_env_sim - phases_audio, color="red", alpha=0.5, label="Sim")
+    plt.title(event_id)
+
+plt.figure(1)
+plt.suptitle("Real Time")
+plt.figure(2)
+plt.suptitle("Simulation")
+
+plt.show()
+print("h")
 
 
